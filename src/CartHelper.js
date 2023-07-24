c83990fe354e5122b73099756f976666ffa4e1eb
riffelj <jriffel@xpanxion.com>
2023-07-24 12:41:23 -0500

    use swr mutations in the shopping cart

--- src/CartHelper.js
+++ src/CartHelper.js
@@ -1,98 +1,80 @@
-import axios from "axios";
+import { mutate } from "swr";
 import UuidStore from "./UuidStore";
 
-async function _getCart() {
-    const response = await axios.get(
-        "http://localhost:3333/cart", {
-            headers: { 
-                "X-SESSION-TOKEN": UuidStore.value
-            }
-        });
-    
-    let cart = response.data;
-    return cart;
-}
-
 export function addCart(id) {
-    return async function addCartThunk(dispatch, getState) {
-        await axios.post(
-            "http://localhost:3333/cart",
-            {
-                id: id
+    fetch(
+        "http://localhost:3333/cart", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+                "X-SESSION-TOKEN": UuidStore.value
             },
-            {
-                headers: { 
-                    "Content-Type": "application/json",
-                    "X-SESSION-TOKEN": UuidStore.value
-                }
-            });
-        let cart = await _getCart();
-        dispatch({ type: "refresh", payload: cart });
-    } 
+            body: JSON.stringify({
+                id: id
+            })
+        }).then(() => {
+            mutate("http://localhost:3333/cart");
+        });
 }
 
 export function updateCart(id, quantity) {
-    return async function updateCartThunk(dispatch, getState) {
-        if (quantity === 0) {
-            await axios.delete(
-                "http://localhost:3333/cart", {
-                    headers: { 
-                        "Content-Type": "application/json",
-                        "X-SESSION-TOKEN": UuidStore.value
-                    },
-                    data: {
-                        id: id
-                    }
-                });
-        } else {
-            await axios.patch(
-                "http://localhost:3333/cart", 
-                {
+    if (quantity === 0) {
+        fetch(
+            "http://localhost:3333/cart", {
+                method: "DELETE",
+                headers: { 
+                    "Content-Type": "application/json",
+                    "X-SESSION-TOKEN": UuidStore.value
+                },
+                body: JSON.stringify({
+                    id: id
+                })
+            }).then(() => {
+                mutate("http://localhost:3333/cart");
+            });
+    } else {
+        fetch(
+            "http://localhost:3333/cart", {
+                method: "PATCH",
+                headers: { 
+                    "Content-Type": "application/json",
+                    "X-SESSION-TOKEN": UuidStore.value
+                },
+                body: JSON.stringify({
                     id: id,
                     quantity: quantity
-                },
-                {
-                    headers: { 
-                        "Content-Type": "application/json",
-                        "X-SESSION-TOKEN": UuidStore.value
-                    }
-                });    
-        }
-
-        let cart = await _getCart();
-        dispatch({ type: "refresh", payload: cart });
+                })
+            }).then(() => {
+                mutate("http://localhost:3333/cart");
+            });    
     }
 }
 
 export function deleteCart(id) {
-    return async function deleteCartThunk(dispatch, getState) {
-        await axios.delete(
-            "http://localhost:3333/cart", {
-                headers: { 
-                    "Content-Type": "application/json",
-                    "X-SESSION-TOKEN": UuidStore.value
-                },
-                data: {
-                    id: id
-                }
-            });
-            
-        let cart = await _getCart();
-        dispatch({ type: "refresh", payload: cart });
-    } 
+    fetch(
+        "http://localhost:3333/cart", {
+            method: "DELETE",
+            headers: { 
+                "Content-Type": "application/json",
+                "X-SESSION-TOKEN": UuidStore.value
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        }).then(() => {
+            mutate("http://localhost:3333/cart");
+        });
 }
 
 export function clearCart() {
-    return async function deleteCartThunk(dispatch, getState) {
-        await axios.delete(
-            "http://localhost:3333/cart", {
-                headers: { 
-                    "Content-Type": "application/json",
-                    "X-SESSION-TOKEN": UuidStore.value
-                }
-            });
-            
-        let cart = await _getCart();
-        dispatch({ type: "refresh", payload: cart });
-    } 
+    fetch(
+        "http://localhost:3333/cart", {
+            method: "DELETE",
+            headers: { 
+                "Content-Type": "application/json",
+                "X-SESSION-TOKEN": UuidStore.value
+            }
+        }).then(() => {
+            mutate("http://localhost:3333/cart");
+        });
 }
--- src/Eventitem.js
+++ src/Eventitem.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { useMutation } from "react-query";
 import { addCart } from "./CartHelper";
 
 export default function Eventitem({ event }) {
-  const dispatch = useDispatch();
-  const mutation = useMutation(
-    id => dispatch(addCart(id)),
-    {
-      onSuccess: () => {
-        setTimeout(() => { mutation.reset(); }, 2000);
-      }
-    }
-  )
-
   return (
     <tr className="align-middle">
       <td>
@@ -33,11 +21,8 @@ export default function Eventitem({ event }) {
       </td>
       <td className="max-50">
         <button type="button" className="btn btn-primary btn-primary-themed btn-md font-upper" 
-          onClick={() => mutation.mutate(event.id)}>Add to
+          onClick={() => addCart(event.id)}>Add to
               Cart</button>
-        { mutation.isSuccess ? 
-          <span className="bi bi-bag-check-fill font-xxl ms-2 fadeout"></span> :
-          <span className="bi bi-bag-check-fill text-white font-xxl ms-2 fadeout"></span>}
       </td>
     </tr>
   );
